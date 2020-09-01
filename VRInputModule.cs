0847ec341b81281a9c34aeb5ad0ced4829deb97a
YoikaMinato <r.sun0302@gmail.com>
2020-09-01 20:55:06 +0900

    Canvas Pointer_2

---
+++ VRInputModule.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class VRInputModule : BaseInputModule
+{
+    public Camera m_Camera;
+    public SteamVR_Input_Sources m_TargetSource;
+    public SteamVR_Action_Boolean m_ClickAction;
+
+    private GameObject m_CurrentObject = null;
+    private PointerEventData m_Data = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        m_Data = new PointerEventData(eventSystem);
+    }
+
+    public override void Process()
+    {
+        // Reset data, set camera
+        m_Data.Reset();
+        m_Data.position = new Vector3(m_Camera.pixelWidth / 2, m_Camera.pixelHeight / 2);
+
+        // Raycast
+        eventSystem.RaycastAll(m_Data, m_RaycastResultCache);
+        m_Data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        m_CurrentObject = m_Data.pointerCurrentRaycast.gameObject;
+
+        // Clear
+        m_RaycastResultCache.Clear();
+
+        // Hover
+        HandlePointerExitAndEnter(m_Data, m_CurrentObject);
+
+        // Press
+        if (m_ClickAction.GetStateDown(m_TargetSource))
+        {
+            ProcessPress(m_Data);
+        }
+
+        // Release
+        if (m_ClickAction.GetStateUp(m_TargetSource))
+        {
+            ProcessRelease(m_Data);
+        }
+
+        //throw new System.NotImplementedException();
+    }
+
+    public PointerEventData GetData()
+    {
+        return m_Data;
+    }
+
+    private void ProcessPress(PointerEventData data)
+    {
+
+    }
+
+    private void ProcessRelease(PointerEventData data)
+    {
+
+    }
+}
